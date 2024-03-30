4d0311b863ce90d9f530288a5fd501bddcdd18c2
ayoub <aybouras@student.1337.ma>
2024-03-30 02:42:20 +0000

    prac some expressJS

---
+++ expressJs/expressTest1/routes/users.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.send('User List');
+});
+
+router.get('/new', (req, res) => {
+    res.send('New User!');
+});
+
+router.post('/', (req, res) => {
+    res.send('Create User!');
+}
+);
+
+router.get('/:id', (req, res) => {
+    res.send(`User ${req.params.id}`);
+}
+);
+
+
+
+module.exports = router;
--- expressJs/expressTest1/server.js
+++ expressJs/expressTest1/server.js
@@ -4,6 +4,8 @@ const port = 3000;
 
 app.set('view engine', 'ejs');
 
+app.use(logger);
+
 app.get('/hello', (req, res) => {
     // res.send('Hello World!');
     // res.json({ message: 'Hello World!' });
@@ -11,8 +13,15 @@ app.get('/hello', (req, res) => {
     //  res.status(404).send("Page Not Foundddd");
     // res.render('index', { title: 'Hey', message: 'Hello there!'});
     res.render('index', {txt: "from /view"});
-
     });
+
+const userRouter = require('./routes/users');
+app.use('/users', userRouter);
+
+function logger(req, res, next) {
+    console.log('Route:', req.originalUrl);
+    next();
+}
 
 app.listen(port, () => {
     console.log(`Test app listening at http://localhost:${port}`);
